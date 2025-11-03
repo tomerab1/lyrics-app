e3fc6363e2586b724558977423c52fb911d8ddbc
Tomer <tomer1452002@gmail.com>
2025-11-04 01:16:53 +0200

    Refactor: refactored main.go to work with gcloud

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -14,52 +14,64 @@ import (
 	"github.tomerab1/todo-api/internal/httpserver"
 )
 
-func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	}))
+func getenv(k, def string) string {
+	if v := os.Getenv(k); v != "" {
+		return v
+	}
+	return def
+}
 
-	if err := godotenv.Load(); err != nil {
-		logger.Error("godotenv load failed", "err", err)
-		panic(".env load failed")
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	_ = godotenv.Load()
+
+	mongoURI := getenv("MONGODB_URI", "")
+	if mongoURI == "" {
+		logger.Warn("MONGODB_URI is empty")
 	}
 
-	var (
-		SERVER_ADDR = os.Getenv("SERVER_ADDR")
-		MONGO_ADDR  = os.Getenv("MONGO_ADDR")
-	)
+	port := getenv("PORT", "8080")
+	addr := ":" + port
 
-	app, err := app.New(logger, MONGO_ADDR)
+	app, err := app.New(logger, mongoURI)
 	if err != nil {
 		logger.Error("failed to create app", "err", err)
-		panic("failed to create app")
+		os.Exit(1)
 	}
 
-	srv := http.Server{
-		Addr:    SERVER_ADDR,
-		Handler: httpserver.New(app),
-	}
-	logger.Info("server is running", "addr", srv.Addr)
+	handler := httpserver.New(app)
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	mux := http.NewServeMux()
+	mux.Handle("/", handler)
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+
+	logger.Info("server starting", "addr", addr)
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logger.Error("Could not listen and serve", "addr", srv.Addr, "err", err.Error())
-			panic("failed to listen and serve")
+			logger.Error("listen and serve failed", "err", err)
+			os.Exit(1)
 		}
 	}()
 
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	logger.Info("shutting down the server")
+	logger.Info("shutting down")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
 	if err := srv.Shutdown(ctx); err != nil {
-		logger.Info("server shutdown failed", "err", err.Error())
+		logger.Error("server shutdown failed", "err", err)
 	}
-
-	logger.Info("server gracefully stopped.")
+	logger.Info("server stopped")
 }
